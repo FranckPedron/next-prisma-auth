3dc0b8a04a12e020245f7187b103dea4f47e5cf9
FranckPedron <franckyp1+rapports_git@gmail.com>
2025-01-22 19:17:03 +0100

    Config providers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import authConfig from "@/lib/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+   session: {strategy: 'jwt'},
+    ...authConfig
 })
