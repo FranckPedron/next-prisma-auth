58ecd819eeded0d362ab39575c5f15aea04e15b6
FranckPedron <franckyp1+rapports_git@gmail.com>
2025-01-27 19:01:36 +0100

    Connexion à la base de données

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import authConfig from "@/lib/auth.config";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {prisma} from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-   session: {strategy: 'jwt'},
+    adapter: PrismaAdapter(prisma),
+    session: {strategy: 'jwt'},
     ...authConfig
 })
